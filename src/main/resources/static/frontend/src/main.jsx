6379417829767c8e4f7228454033bbb5b3a45bca
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2026-04-14 16:50:28 +0100

    setting up the main.jsx requirements

--- src/main/resources/static/frontend/src/main.jsx
+++ src/main/resources/static/frontend/src/main.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './main.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './main.css';
 
-createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-app">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
