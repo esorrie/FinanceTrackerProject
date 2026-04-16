363d2536f9a219e7668cd8de8d5357f59447df4d
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2026-04-16 14:37:53 +0100

    templated portfolio page

---
+++ src/main/resources/static/frontend/src/pages/Portfolio.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import "../css/Portfolio.css";
+
+const Portfolio = () => {
+
+
+
+    return (
+        <div className="portfolioMainContainer">
+            <div className="portfolioPreviewContainer">
+                <div className="portfolioHeader"> Portfolio</div>
+                <div className="portfolioValue"> Portfolio Value </div>
+                <div className="portfolioPerformance"> Portfolio performance </div>
+                
+                <div className="portfolioContentContainer">
+                    <div className="portfolioGraphContainer">
+                        <div className="portfolioGraph"> Portfolio graph </div>
+                    </div>
+
+                    <div className="portfolioAssetsContainer">
+                        <div className="portfolioAssetsHeader"> Investments </div>
+                        <div className="portfolioAssetsListContainer">
+                            <div className="portfolioAssetsList"> Asset 1</div>
+                            <div className="portfolioAssetsList"> Asset 2</div>
+                            <div className="portfolioAssetsList"> Asset 3</div>
+                            <div className="portfolioAssetsList"> Asset 4</div>
+                            <div className="portfolioAssetsList"> Asset 5</div>
+                            <div className="portfolioAssetsList"> Asset 1</div>
+                            <div className="portfolioAssetsList"> Asset 2</div>
+                            <div className="portfolioAssetsList"> Asset 3</div>
+                            <div className="portfolioAssetsList"> Asset 4</div>
+                            <div className="portfolioAssetsList"> Asset 5</div>
+                            <div className="portfolioAssetsList"> Asset 1</div>
+                            <div className="portfolioAssetsList"> Asset 2</div>
+                            <div className="portfolioAssetsList"> Asset 3</div>
+                            <div className="portfolioAssetsList"> Asset 4</div>
+                            <div className="portfolioAssetsList"> Asset 5</div>
+                        </div>
+                    </div>
+                    
+                    <div className="portfolioAllocationContainer">
+                        <div className="portfolioAllocationHeader"> Allocations </div>
+                        <div className="portfolioAllocationListContainer">
+                            <div className="portfolioAllocationList"> Allocation 1</div>
+                            <div className="portfolioAllocationList"> Allocation 2</div>
+                            <div className="portfolioAllocationList"> Allocation 3</div>
+                            <div className="portfolioAllocationList"> Allocation 4</div>
+                            <div className="portfolioAllocationList"> Allocation 5</div>
+                            <div className="portfolioAllocationList"> Allocation 1</div>
+                            <div className="portfolioAllocationList"> Allocation 2</div>
+                            <div className="portfolioAllocationList"> Allocation 3</div>
+                            <div className="portfolioAllocationList"> Allocation 4</div>
+                            <div className="portfolioAllocationList"> Allocation 5</div>
+                            <div className="portfolioAllocationList"> Allocation 1</div>
+                            <div className="portfolioAllocationList"> Allocation 2</div>
+                            <div className="portfolioAllocationList"> Allocation 3</div>
+                            <div className="portfolioAllocationList"> Allocation 4</div>
+                            <div className="portfolioAllocationList"> Allocation 5</div>
+                            <div className="portfolioAllocationList"> Allocation 1</div>
+                            <div className="portfolioAllocationList"> Allocation 2</div>
+                            <div className="portfolioAllocationList"> Allocation 3</div>
+                            <div className="portfolioAllocationList"> Allocation 4</div>
+                            <div className="portfolioAllocationList"> Allocation 5</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                
+            <div className="portfolioDataContainer">
+                <div className="portfolioData">This is where the portfolio details will be displayed.</div>
+            </div>
+        </div>
+    )
+}
+
+export default Portfolio;
