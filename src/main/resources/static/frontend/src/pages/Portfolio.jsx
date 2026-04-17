e2ef5221d483744b75a9d72e82cf401dbbb5eb80
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2026-04-17 14:35:31 +0100

    changed css styling, added blue box shadow

--- src/main/resources/static/frontend/src/pages/Portfolio.jsx
+++ src/main/resources/static/frontend/src/pages/Portfolio.jsx
@@ -5,38 +5,39 @@ const Portfolio = () => {
 
 
     return (
-        <div className="portfolioMainContainer">
-            <div className="portfolioPreviewContainer">
-                <div className="portfolioHeader"> Portfolio</div>
-                <div className="portfolioValue"> Portfolio Value </div>
-                <div className="portfolioPerformance"> Portfolio performance </div>
-                
-                <div className="portfolioContentContainer">
-                    <div className="portfolioGraphContainer">
-                        <div className="portfolioGraph"> Portfolio graph </div>
-                    </div>
-
-                    <div className="portfolioAssetsContainer">
-                        <div className="portfolioAssetsHeader"> Investments </div>
-                        <div className="portfolioAssetsListContainer">
-                            <div className="portfolioAssetsList"> Asset 1 </div>
-                        </div>
-                    </div>
+        <>
+            <div className="portfolioMainContainer">
+                <div className="portfolioPreviewContainer">
+                    <div className="portfolioValue"> Portfolio Value </div>
+                    <div className="portfolioPerformance"> Portfolio performance </div>
                     
-                    <div className="portfolioAllocationContainer">
-                        <div className="portfolioAllocationHeader"> Allocations </div>
-                        <div className="portfolioAllocationListContainer">
-                            <div className="portfolioAllocationList"> Allocation 1</div>
+                    <div className="portfolioContentContainer">
+                        <div className="portfolioGraphContainer">
+                            <div className="portfolioGraph"> Portfolio graph </div>
+                        </div>
 
+                        <div className="portfolioAssetsContainer">
+                            <div className="portfolioAssetsHeader"> Investments </div>
+                            <div className="portfolioAssetsListContainer">
+                                <div className="portfolioAssetsList"> Asset 1 </div>
+                            </div>
+                        </div>
+                        
+                        <div className="portfolioAllocationContainer">
+                            <div className="portfolioAllocationHeader"> Allocations </div>
+                            <div className="portfolioAllocationListContainer">
+                                <div className="portfolioAllocationList"> Allocation 1</div>
+
+                            </div>
                         </div>
                     </div>
                 </div>
+                    
+                <div className="portfolioDataContainer">
+                    <div className="portfolioData">This is where the portfolio details will be displayed.</div>
+                </div>
             </div>
-                
-            <div className="portfolioDataContainer">
-                <div className="portfolioData">This is where the portfolio details will be displayed.</div>
-            </div>
-        </div>
+        </>
     )
 }
 
