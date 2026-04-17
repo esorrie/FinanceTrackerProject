55a70591f04a59ac975bfdd9998d2cd5dbfe635f
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2026-04-17 15:36:46 +0100

    fixed syntax

--- src/main/resources/static/frontend/src/pages/Portfolio.jsx
+++ src/main/resources/static/frontend/src/pages/Portfolio.jsx
@@ -5,39 +5,40 @@ const Portfolio = () => {
 
 
     return (
-        <>
-            <div className="portfolioMainContainer">
-                <div className="portfolioPreviewContainer">
-                    <div className="portfolioValue"> Portfolio Value </div>
-                    <div className="portfolioPerformance"> Portfolio performance </div>
-                    
-                    <div className="portfolioContentContainer">
-                        <div className="portfolioGraphContainer">
-                            <div className="portfolioGraph"> Portfolio graph </div>
+            <>
+                <div className="portfolioMainContainer">
+                    <div className="portfolioPreviewContainer">
+                        <div className="portfolioValue"> Portfolio Value </div>
+                        <div className="portfolioPerformance"> Portfolio performance </div>
+                        
+                        <div className="portfolioContentContainer">
+                            <div className="portfolioGraphContainer">
+                                <div className="portfolioGraph"> Portfolio graph </div>
+                            </div>
                         </div>
 
-                    <div className="portfolioAssetsContainer">
-                        <div className="portfolioAssetsHeader"> Investments </div>
-                        <div className="portfolioAssetsListContainer">
-                            <div className="portfolioAssetsList"> Asset 1 </div>
+                        <div className="portfolioAssetsContainer">
+                            <div className="portfolioAssetsHeader"> Investments </div>
+                            <div className="portfolioAssetsListContainer">
+                                <div className="portfolioAssetsList"> Asset 1 </div>
+                            </div>
+                        </div>
+                        
+                        <div className="portfolioAllocationContainer">
+                            <div className="portfolioAllocationHeader"> Allocations </div>
+                            <div className="portfolioAllocationListContainer">
+                                <div className="portfolioAllocationList"> Allocation 1</div>
+
+                            </div>
                         </div>
                     </div>
-                    
-                    <div className="portfolioAllocationContainer">
-                        <div className="portfolioAllocationHeader"> Allocations </div>
-                        <div className="portfolioAllocationListContainer">
-                            <div className="portfolioAllocationList"> Allocation 1</div>
-
-                        </div>
+                        
+                    <div className="portfolioDataContainer">
+                        <div className="portfolioData">This is where the portfolio details will be displayed.</div>
                     </div>
                 </div>
-                    
-                <div className="portfolioDataContainer">
-                    <div className="portfolioData">This is where the portfolio details will be displayed.</div>
-                </div>
-            </div>
-        </>
-    )
-}
+            </>
+        )
+    }
 
-export default Portfolio;
+    export default Portfolio;
